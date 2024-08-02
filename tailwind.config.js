445c9f0283b0fa834cf0732df4845ceb693a3872
Florian <93609174+Xeferis@users.noreply.github.com>
2024-08-02 06:10:00 +0000

    Added Background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.html","./index.html","./js/**/*.js"],
   theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'gray-dark': '#161616'
+    },
     extend: {},
   },
   plugins: [],
