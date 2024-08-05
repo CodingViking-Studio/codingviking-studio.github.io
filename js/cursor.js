0bf7cc7408458cb5e67e35394bfc06cc8953dbdc
Flo <93609174+Xeferis@users.noreply.github.com>
2024-08-05 19:06:57 +0200

    Added cursor

---
+++ js/cursor.js
@@ -0,0 +1,34 @@
+var cursor = document.querySelector('.cursor');
+var a_elem = Array.from(document.querySelectorAll('a'));
+
+function move(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+}
+
+window.addEventListener('DOMContentLoaded', move);
+window.addEventListener('mousemove', move);
+document.addEventListener('mouseover', () => cursor.style.opacity = 1);
+document.addEventListener('mouseout', () => cursor.style.opacity = 0);
+
+document.addEventListener('mousedown', function(e){
+    cursor.classList.add("click");
+  }
+);
+
+document.addEventListener('mouseup', function(e){
+    cursor.classList.remove("click");
+  }
+);
+
+a_elem.forEach(elem => {
+    elem.addEventListener('mouseover', function(x) {
+    cursor.classList.add('hover_c');
+  })
+//To remove the class when it doesn't hover the text
+    elem.addEventListener('mouseleave', function (x) {
+    cursor.classList.remove('hover_c');
+  })
+});
