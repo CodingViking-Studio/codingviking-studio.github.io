2a48ff7b18b5eba3e65b71ca1476f630e1596d9a
Florian <93609174+Xeferis@users.noreply.github.com>
2024-08-06 06:22:01 +0000

    Adjusted custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,12 @@
 module.exports = {
   content: ["./src/**/*.html","./index.html","./js/**/*.js"],
   theme: {
-    colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff5520',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
-      'gray-dark': '#161616'
+    extend: {
+      colors: {
+        'cvs-orange': '#ff5520',
+        'cvs-dark': '#161616'
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
