7d58397172676e9059193d13650fe33e887676d8
Flo <93609174+Xeferis@users.noreply.github.com>
2024-08-07 20:02:05 +0200

    Fixed opacity bug

--- js/basic.js
+++ js/basic.js
@@ -1,16 +1,27 @@
+window.addEventListener('load', function () {
+    console.log("Page Loaded");
+    console.log(window.scrollY)
+})
+
 document.onscroll = function () {
     if (window.scrollY > window.innerHeight*0.85) {
-        document.getElementById("scroll_arrow").style.opacity = 0;
-        document.getElementById("para_scene").style.opacity = 0;
+        document.getElementById("scroll_arrow").classList.remove("opacity-100");
+        document.getElementById("para_scene").classList.remove("opacity-100");
+        document.getElementById("scroll_arrow").classList.add("opacity-0");
+        document.getElementById("para_scene").classList.add("opacity-0");
     }
     else {
-        document.getElementById("scroll_arrow").style.opacity = 1;
-        document.getElementById("para_scene").style.opacity = 1;
+        document.getElementById("scroll_arrow").classList.remove("opacity-0");
+        document.getElementById("para_scene").classList.remove("opacity-0");
+        document.getElementById("scroll_arrow").classList.add("opacity-100");
+        document.getElementById("para_scene").classList.add("opacity-100");
     }
     if (window.scrollY > window.innerHeight * 0.2) {
-        document.getElementById("s2").style.opacity = 1;
+        document.getElementById("s2").classList.remove("opacity-0");
+        document.getElementById("s2").classList.add("opacity-100");
     }
     else {
-        document.getElementById("s2").style.opacity = 0;
+        document.getElementById("s2").classList.remove("opacity-100");
+        document.getElementById("s2").classList.add("opacity-0");
     }
 }
