f3ecb600d4f63790de6bddcfb0d1d9f018601102
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-26 09:27:57 +0000

    Added data

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,4 @@
-<!doctype html>
-<html class="h-full scroll-smooth">
-<head>
-  <meta http-equiv='cache-control' content='no-cache'> 
-  <meta http-equiv='expires' content='0'> 
-  <meta http-equiv='pragma' content='no-cache'>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-  <link rel="manifest" href="./manifest.json">
-  <meta name="msapplication-TileColor" content="#161616">
-  <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-  <meta name="theme-color" content="#161616">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link href="./css/output.css" rel="stylesheet">
-  <link href="./css/custom.css" rel="stylesheet">
-  <link href="./css/cursor.css" rel="stylesheet">
-  <title>C V S</title>
-</head>
-<body class="bg-cvs-dark w-full min-h-full">
+<template>
   <div class="z-10 cursor"></div>
   <section id="s1" class="flex flex-col justify-center items-center w-full min-h-screen">
     <div data-relative-input="true" id="para_scene" class="fade flex flex-1 justify-center items-center select-none opacity-100">
@@ -174,5 +140,4 @@
     parallaxInstance.hoverOnly=true;
     parallaxInstance.friction(0.2, 0.2);
   </script>
-</body>
-</html>
+</template>
