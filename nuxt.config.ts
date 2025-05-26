b473dc8bdd311d94019059d48ff6e72fa796737b
Florian <93609174+Xeferis@users.noreply.github.com>
2025-05-26 09:16:33 +0000

    Added nuxt data

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  css: ['~/assets/css/cursor.css', '~/assets/css/custom.css', '~/assets/css/input.css', '~/assets/css/output.css']
+})
