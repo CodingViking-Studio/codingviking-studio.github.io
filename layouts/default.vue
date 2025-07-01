e95abd856af077f04a246c3a39fdc0f0f7c60162
Florian <93609174+Xeferis@users.noreply.github.com>
2025-07-01 10:18:10 +0200

    Update default.vue
    Signed-off-by: Florian <93609174+Xeferis@users.noreply.github.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,4 +2,5 @@
   <div>
     <slot />
   </div>
+  <img alt="GitHub Release" src="https://img.shields.io/github/v/release/CodingViking-Studio/codingviking-studio.github.io?display_name=release&style=for-the-badge&color=gray">
 </template>
